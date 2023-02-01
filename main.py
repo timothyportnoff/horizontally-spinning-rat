4a3a3936177bfd338a6532f0cb9eaa8c7bd42af9
= <tp@gmail.com>
2023-02-01 02:02:52 -0800

    ::

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from time import sleep
 
 #CONSTANTS
 POTENTIOMMETER = 0
-BUTTON = 0
+START_BUTTON = 26
+STOP_BUTTON = 0
 BUZZER = 0
 MOTOR = 0
 YELLOW = 17
@@ -16,24 +17,29 @@ GREEN = 0
 BLUE = 19
 RED = 0
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(YELLOW, GPIO.OUT)
-GPIO.setup(BLUE, GPIO.OUT)
-GPIO.setup(RED, GPIO.IN)
-GPIO.setup(GREEN, GPIO.IN)
+#SETUP
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(YELLOW, GPIO.OUT)
+    GPIO.setup(BLUE, GPIO.OUT)
+    GPIO.setup(RED, GPIO.IN)
+    GPIO.setup(GREEN, GPIO.IN)
+
 #GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # TODO set in as off when initializing
 GPIO.setup(26, GPIO.IN)
 
 #main function
 if __name__ =="__main__":
-    #Welcome
+    #Welcome, and setup
     print("Welcome to horizontally spinning rat")
+    setup()
 
     #Do constant button press checking
     while True:
-        if button_is_pressed(STOP BUTTON) GPIO.input(26) == GPIO.HIGH:
+        if button_is_pressed(START_BUTTON) GPIO.input(START_BUTTON) == GPIO.HIGH:
             print("button pressed!")
             led_on(YELLOW)
+        else if button_is_pressed(STOP_BUTTON) # Sleep for half a second? FIXME
         else sleep(0.5) # Sleep for half a second? FIXME
 
     #Exit cleanly
