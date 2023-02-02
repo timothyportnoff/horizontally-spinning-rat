f5d3969f4ea11ff50165352a17b233b42f0c4355
= <tp@gmail.com>
2023-02-02 00:05:04 -0800

    Added ownership for group

--- main.py
+++ main.py
@@ -10,38 +10,52 @@ from time import sleep
 POTENTIOMMETER = 0
 START_BUTTON = 26
 STOP_BUTTON = 0
-BUZZER = 0
-MOTOR = 0
-YELLOW = 0
-GREEN = 0
-BLUE = 0
-RED = 0
+#BUZZER = 0
+#KNOB = 0
+#MOTOR = 0
+#YELLOW = 0
+#GREEN = 0
+#BLUE = 0
+#RED = 0
 
 #SETUP
 def setup():
+    #GPIO settings
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(YELLOW, GPIO.OUT)
-    GPIO.setup(BLUE, GPIO.OUT)
-    GPIO.setup(RED, GPIO.IN)
-    GPIO.setup(GREEN, GPIO.IN)
-    #GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # TODO set in as off when initializing
-    GPIO.setup(26, GPIO.IN)
+
+    #tim TODO
+    #GPIO.setup(START_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set in as off when initializing
+    GPIO.setup(START_BUTTON, GPIO.IN)
+
+    #mora TODO
+    #
+
+    #edgar TODO
+    #
+
+    #lara TODO
+    #
+
+    #LEDS
+    #GPIO.setup(YELLOW, GPIO.OUT)
+    #GPIO.setup(BLUE, GPIO.OUT)
+    #GPIO.setup(RED, GPIO.IN)
+    #GPIO.setup(GREEN, GPIO.IN)
 
 #main function
 if __name__ =="__main__":
     #Welcome, and setup
+    setup()
     print("Welcome to horizontally spinning rat")
     print("Press the button to begin.")
-    setup()
 
     #Do constant button press checking
     while True:
         if button_is_pressed(START_BUTTON): #GPIO.input(START_BUTTON) == GPIO.HIGH:
             print("start button pressed!")
-            #led_on(YELLOW)
-        elif button_is_pressed(STOP_BUTTON): # Sleep for half a second? FIXME
-            print("stop button pressed!")
-        else: sleep(0.9) # Sleep for a second? FIXME
+        #elif button_is_pressed(STOP_BUTTON): # Sleep for half a second? FIXME
+            #print("stop button pressed!")
+        else: sleep(1) #Sleep for a second? FIXME
 
     #Exit cleanly
     GPIO.cleanup()
